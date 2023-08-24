7b52d93c7458df772187e71aa956a6f0bd38fd55
MauronMP <pablo.mopi@gmail.com>
2023-08-24 23:02:24 +0200

    :bug: Bug(#7): Fixed

--- app/frontend/src/scenes/worker/removeWorker.jsx
+++ app/frontend/src/scenes/worker/removeWorker.jsx
@@ -7,55 +7,55 @@ import { API_BASE_URL } from "../../config.js";
 
 const HomeComponent = () => {
 
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const { id } = useParams();
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const { id } = useParams();
 
-  /**
-   * 
-   * Remove worker function, removes the worker from the database given its id.
-   * if the response is ok, redirects to the worker page.
-   * 
-   */
-  const handleDeleteUser = () => {
-    fetch(`${API_BASE_URL}/worker/${id}`, {
-      method: "DELETE",
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-      })
-      .then((data) => {
-        window.location.href = "/worker";
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
-  };
+    /**
+     * 
+     * Remove worker function, removes the worker from the database given its id.
+     * if the response is ok, redirects to the worker page.
+     * 
+     */
+    const handleDeleteUser = () => {
+        fetch(`${API_BASE_URL}/worker/${id}`, {
+            method: "DELETE",
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+            })
+            .then((data) => {
+                window.location.href = "/worker";
+            })
+            .catch((error) => {
+                throw new Error(error);
+            });
+    };
 
-  // Render the button to remove the worker
-  return (
-    <Box m="20px" >
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <Header title={`You are going to delete the worker with ${id}`} subtitle="Are you sure you want to delete it?" />
-      </Box>
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <Button
-          sx={{
-            backgroundColor: colors.blueAccent[700],
-            color: colors.grey[100],
-            fontSize: "14px",
-            fontWeight: "bold",
-            padding: "10px 20px",
-          }}
-          onClick={handleDeleteUser}>
-          Remove worker
-        </Button>
-      </Box>
-    </Box>
-  );
+    // Render the button to remove the worker
+    return (
+        <Box m="20px" >
+            <Box display="flex" justifyContent="center" alignItems="center">
+                <Header title={`You are going to delete the worker with ${id}`} subtitle="Are you sure you want to delete it?" />
+            </Box>
+            <Box display="flex" justifyContent="center" alignItems="center">
+                <Button
+                    sx={{
+                        backgroundColor: colors.blueAccent[700],
+                        color: colors.grey[100],
+                        fontSize: "14px",
+                        fontWeight: "bold",
+                        padding: "10px 20px",
+                    }}
+                    onClick={handleDeleteUser}>
+                    Remove worker
+                </Button>
+            </Box>
+        </Box>
+    );
 };
 
 export default HomeComponent;
